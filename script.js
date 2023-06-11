620ac23a5e4fa1857defc2e790cd3ad18cccafaa
harshal1507 <hgpkk12@gmail.com>
2023-06-11 22:18:38 +0530

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 gsap.to(".imagecontainer",{
     width:"100%",
     ease: Expo.easeInOut,
-    stagger: 2
+    stagger: 2,
+    repeat: -2
 })
 
 gsap.to(".text h1",{
      ease: Expo.easeInOut,
      stagger: 2,
-     top: 0
+     top: 0,
+     repeat: -2
 })
 
 gsap.to(".text h1",{
     delay: 2,
     ease: Expo.easeInOut,
     stagger: 2,
-    top: "-100%"
+    top: "-100%",
+    repeat: -2
 })
