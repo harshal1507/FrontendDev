08b1ede1bc4d81ed9bc3d0ac7e1dd0669fcab58e
harshal1507 <hgpkk12@gmail.com>
2023-06-11 22:05:04 +0530

    final animation without inifinite loop

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to(".imagecontainer",{
+    width:"100%",
+    ease: Expo.easeInOut,
+    stagger: 2
+})
+
+gsap.to(".text h1",{
+     ease: Expo.easeInOut,
+     stagger: 2,
+     top: 0
+})
+
+gsap.to(".text h1",{
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%"
+})
